818c2c70f8f5af1d3ea6db1c1cec6ace68195528
saikiran <saikiran.panga@winfosolutions.com>
2025-06-16 22:59:38 +0530

    rollback to default code

--- src/functions/TestActions.js
+++ src/functions/TestActions.js
@@ -1,43 +1,26 @@
 const { app } = require('@azure/functions');
-const { BlobServiceClient } = require('@azure/storage-blob');
+
 require('dotenv').config();
  
-app.http('httpTrigger1', {
+app.http('TestActions', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`HTTP function processed request for URL "${request.url}"`);
  
-       
-        // Only do blob storage if POST and body contains data
-        if (request.method === 'POST') {
-            try {
-                const body = await request.json(); // Parse JSON from body
-                const content = body?.data || 'No content received';
-                const filename = body?.filename || 'default.txt';
- 
-                const AZURE_STORAGE_CONNECTION_STRING = process.env.Azure_connection_string;
-                const containerName = process.env.Azure_container||'export';  // Ensure this exists
- 
-                const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-                const containerClient = blobServiceClient.getContainerClient(containerName);
- 
-                const blockBlobClient = containerClient.getBlockBlobClient(filename);
-                await blockBlobClient.upload(content, Buffer.byteLength(content));
- 
-                responseMessage = `File '${filename}' saved to Azure Blob Storage Success.`;
-            } catch (error) {
-                context.log.error('Error uploading to Blob:', error);
-                return {
-                    status: 500,
-                    body: `Error : ${error.message}`
-                };
-            }
-        }
- 
+       const name=request.body?.name;
+       if(!name){
         return {
-            status: 200,
-            body: responseMessage
-        };
+            status:500,
+            body:`please pass a name in the body`
+        }
+        
+       }
+       return {
+        status:200,
+        body:JSON.stringify(`Hello ${name} how are you`)
+       }
+        
+        
     }
 });
